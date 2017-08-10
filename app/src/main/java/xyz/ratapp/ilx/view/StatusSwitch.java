d1763d819051254c98250fe96a98a54a45dcb729
Diaglyonok <diaglyonok@yandex.ru>
2017-08-10 15:17:28 +0300

    swipe_changed

--- app/src/main/java/xyz/ratapp/ilx/view/StatusSwitch.java
+++ app/src/main/java/xyz/ratapp/ilx/view/StatusSwitch.java
@@ -23,7 +23,6 @@ public class StatusSwitch extends Switch {
         setText(R.string.offline);
         setOnCheckedChangeListener(listener);
         setTextAppearance(context, R.style.TextAppearance_AppCompat_Widget_ActionBar_Title_Inverse);
-        setTextSize(getTextSize() - 24);
     }
 
     private void setText() {
